97b0a5c9e4cb6a07ca34b4ca289892f5cbfe3aad
Nazim Boudeffa <boudeffa@gmail.com>
2020-10-25 17:15:26 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Novelator
+
+A portable text editor to write your novels based on [Left](https://github.com/hundredrabbits/Left)
+
+## Install & Run
+
+```
+git clone https://github.com/nazimboudeffa/novelator.git
+cd novelator/app/
+npm install
+npm start
+```
+
+## Build
+
+```
+git clone https://github.com/nazimboudeffa/novelator.git
+cd novelator/app/
+npm run build_win
+```
---
+++ app/main.js
@@ -0,0 +1,97 @@
+const {
+  app,
+  BrowserWindow,
+  webFrame,
+  Menu
+} = require('electron')
+const path = require('path')
+const url = require('url')
+const shell = require('electron').shell
+
+let isShown = true
+
+app.win = null
+
+app.on('ready', () => {
+  app.win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 640,
+    minHeight: 360,
+    backgroundColor: '#eee',
+    icon: path.join(__dirname, {
+      darwin: 'icon.icns',
+      linux: 'icon.png',
+      win32: 'icon.ico'
+    } [process.platform] || 'icon.ico'),
+    resizable: true,
+    frame: (process.argv.indexOf('frameless') == -1) && (process.platform !== 'darwin'),
+    skipTaskbar: process.platform === 'darwin',
+    autoHideMenuBar: process.platform === 'darwin',
+    webPreferences: {
+      zoomFactor: 1.0,
+      nodeIntegration: true,
+      backgroundThrottling: false
+    }
+  })
+
+  app.win.loadURL(`file://${__dirname}/sources/index.html`)
+  // app.inspect()
+
+  app.win.on('closed', () => {
+    win = null
+    app.quit()
+  })
+
+  app.win.on('hide', function() {
+    isShown = false
+  })
+
+  app.win.on('show', function() {
+    isShown = true
+  })
+
+  app.on('window-all-closed', () => {
+    app.quit()
+  })
+
+  app.on('activate', () => {
+    if (app.win === null) {
+      createWindow()
+    } else {
+      app.win.show()
+    }
+  })
+})
+
+app.inspect = function() {
+  app.win.toggleDevTools()
+}
+
+app.toggleFullscreen = function() {
+  app.win.setFullScreen(!app.win.isFullScreen())
+}
+
+app.toggleVisible = function() {
+  if (process.platform === 'darwin') {
+    if (isShown && !app.win.isFullScreen()) {
+      app.win.hide()
+    } else {
+      app.win.show()
+    }
+  } else {
+    if (!app.win.isMinimized()) {
+      app.win.minimize()
+    } else {
+      app.win.restore()
+    }
+  }
+}
+
+app.injectMenu = function(menu) {
+  try {
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menu))
+  } catch (err) {
+    console.warn('Cannot inject menu.')
+  }
+}
